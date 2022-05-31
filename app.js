ff6d1ba241e6c5a98160de2e5cbdf3b2178112a7
rdxdheeraj75 <dheerajp748@gmail.com>
2022-05-31 23:27:13 +0530

    Salting and Hashing Passwords with bcrypt

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+
+const saltRounds = 10;
 
 const app = express();
 
@@ -25,23 +27,26 @@ const userSchema = new mongoose.Schema({
 const User = new mongoose.model("user", userSchema);
 
 app.post("/register", function(req, res){
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password)
-  });
 
-  newUser.save(function(err){
-    if(err)
-    console.log(err);
-    else
-    res.render("secrets");
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+
+    newUser.save(function(err){
+      if(err)
+      console.log(err);
+      else
+      res.render("secrets");
+    });
   });
 
 });
 
 app.post("/login", function(req, res){
   const username = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
 
   User.findOne({email: username}, function(err, foundUser){
     if(err)
@@ -52,10 +57,12 @@ app.post("/login", function(req, res){
     {
       if(foundUser)
       {
-        if(foundUser.password === password)
-        {
-          res.render("secrets");
-        }
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+          if(result === true)
+          {
+            res.render("secrets");
+          }
+        });
       }
     }
 
